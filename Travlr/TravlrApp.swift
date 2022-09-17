14fb09feea12eb7e6a41a91cd1b955b74757a6c6
Ishnu Suresh <ishnu50@gmail.com>
2022-09-17 09:24:13 -0400

    Initial Commit

---
+++ Travlr/TravlrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravlrApp.swift
+//  Travlr
+//
+//  Created by Ishnu Suresh on 2022-09-17.
+//
+
+import SwiftUI
+
+@main
+struct TravlrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
